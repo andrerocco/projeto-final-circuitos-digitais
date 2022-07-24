7ba03dab453e5e49df655c144e4f235c433e3f45
Andre Rocco <andre.amaral.rocco@gmail.com>
2022-07-24 18:23:55 -0300

    Modificado datapath

--- datapath.vhd
+++ datapath.vhd
@@ -34,11 +34,14 @@ signal mxsel_msb, mxsel_lsb: std_logic_vector(3 downto 0);
 signal E2_and_X0, notE1_or_R1: std_logic; 
 signal saida_rom0, saida_rom1, saida_rom2, saida_rom3: std_logic_vector(79 downto 0);
 signal sel_cnt, sel_mxledr: std_logic_vector(1 downto 0);
-signal mux_ctrlleft, saida_load_left, mux_ctrlright, saida_load_right: std_logic_vector(7 downto 0);
+signal mux_ctrlleft, saida_load_left, mux_ctrlright, saida_load_right: std_logic_vector(7 downto 0); -- USAR
 signal CLK_1Hz, Sim_1Hz, enable_left, enable_right: std_logic;
 signal SWleft, SWRight: std_logic_vector(15 downto 0);
 
-signal left_penalty, right_penalty: std_logic_vector(7 downto 0);
+signal left_penalty, right_penalty: std_logic_vector(7 downto 0); -- É a saída do primeiro MUX2_1x8 do Counter_time
+
+-- SINAIS CRIADOS
+signal reset_cnt_seq: std_logic;
 
 ----- components
 
@@ -147,7 +150,51 @@ end component;
 
 ------------------------completar os components que faltam------------------------------
 
--- ESCREVER
+component Registrador4Bits is port(
+    CLK: in std_logic;
+	R: in std_logic;
+	E: in std_logic;
+    ValorBinario_in: in std_logic_vector(3 downto 0);
+    ValorBinario_out: out std_logic_vector(3 downto 0)
+    );
+end component;
+
+component Registrador16Bits is port(
+    CLK: in std_logic;
+	R: in std_logic;
+	E: in std_logic;
+    ValorBinario_in: in std_logic_vector(15 downto 0);
+    ValorBinario_out: out std_logic_vector(15 downto 0)
+    );
+end component;
+
+component Counter_time is port(
+    Set: in std_logic; -- A entrada Set define o contador para o valor 99
+	E: in std_logic; -- É o enable e funciona de forma síncrona
+	LOAD: in std_logic_vector(7 downto 0); -- Define qual vai ser o intervalo de variação do valor a cada clock
+	CLK: in std_logic; 
+	tc_lt1: out std_logic; -- A saída "tc less than 1" é ativada quando o valor do contador for menor do que 1
+	T_out: out std_logic_vector(7 downto 0) -- É a saída da contagem
+    );
+end component;
+
+component Counter_round is port(
+    Set: in std_logic; -- A entrada Set define o contador para o valor 15
+	E: in std_logic; -- É o enable e funciona de forma síncrona
+	CLK: in std_logic; 
+	tc_eq0: out std_logic; -- A saída "tc equals 1" é ativada quando o valor do contador for igual a zero
+	T_out: out std_logic_vector(3 downto 0) -- É a saída da contagem
+    );
+end component;
+
+component Counter_seq is port(
+    R: in std_logic; -- A entrada reset define o contador para o valor 0
+	E: in std_logic; -- É o enable e funciona de forma síncrona
+	CLK: in std_logic; 
+	tc_eq4: out std_logic; -- A saída "tc equals 4" é ativada quando o valor do contador for igual a 4 decimal
+	T_out: out std_logic_vector(1 downto 0) -- É a saída da contagem
+    );
+end component;
 
 begin
 
@@ -234,6 +281,95 @@ end_right <= Enter_right; --mas no resto do projeto continuam sendo Enter_left e
 
 
 ------------------------------------ FAZER ----------------------------------------
+
+-- Counter_time para LEFT
+
+enable_left <= (((E3 and CLK_1Hz) and not(Enter_left)) or E4); -- Entrada do enable para o contador do jogador da esquerda
+
+mx1_load_counter_time_LEFT: Mux2_1x8 port map(Control_left, Penalty, "00000000", left_penalty);
+mx2_load_counter_time_LEFT: Mux2_1x8 port map(E4, "11111111", left_penalty, saida_load_left);
+
+Counter_time_LEFT: Counter_time port map(Set => R1, E => enable_left, LOAD => saida_load_left, CLK => CLK, 
+                                         tc_lt1 => end_time_left, T_out => T_left_out);
+
+DecBCD_Counter_LEFT: DecBCD port map(T_left_out, T_left_BCD);
+
+
+
+-- Counter_time para RIGHT
+
+enable_right <= (((E3 and CLK_1Hz) and not(Enter_right)) or E4); -- Entrada do enable para o contador do jogador da direita
+
+mx1_load_counter_time_RIGHT: Mux2_1x8 port map(Control_right, Penalty, "00000000", right_penalty);
+mx2_load_counter_time_RIGHT: Mux2_1x8 port map(E4, "11111111", right_penalty, saida_load_right);
+
+Counter_time_RIGHT: Counter_time port map(Set => R1, E => enable_right, LOAD => saida_load_right, CLK => CLK, 
+                                         tc_lt1 => end_time_right, T_out => T_right_out);
+
+DecBCD_Counter_RIGHT: DecBCD port map(T_right_out, T_right_BCD);
+
+
+
+-- Counter_round
+
+contador_ROUNDS: Counter_round port map(R1, E4, CLK, end_round, X); -- O end_round não é um sinal, mas sim uma saída do datapath
+decodificador_termo_ROUNDS: decoder_termometrico port map (X, termo);
+
+
+
+-- Counter_seq
+
+reset_cnt_seq <= R1 or E5;
+contador_SEQUENCE: Counter_seq port map(reset_cnt_seq, E2, CLK_1Hz, end_sequence, sel_cnt); -- O end_sequence não é um sinal, mas sim uma saída do datapath
+
+
+-- Registradores
+
+REG1: Registrador4Bits port map(CLK, R1, E1, SW(3 downto 0), sel);
+
+not_entl <= not(Enter_left);
+SWleft <= SW(17 downto 10) & S(79 downto 72);
+REG2: Registrador16Bits port map(CLK, R1, not_entl, SWleft, play_left);
+
+not_entr <= not(Enter_right);
+SWright <= SW(7 downto 0) & S(39 downto 32);
+REG3: Registrador16Bits port map(CLK, R1, not_entr, SWright, play_right);
+
+
+
+-- Mux da Penalidade
+
+mux_penalidade: Mux4_1x8 port map(sel(1 downto 0), "11111110", "11111100", "11111010", "11111000", Penalty);
+
+-- Mux do Modo de jogo
+
+mux_memorias: Mux4_1x80 port map(sel(3 downto 2), saida_rom0, saida_rom1, saida_rom2, saida_rom3, S);
+
+-- Muxes das Sequências
+
+mux_seq_RIGHT: Mux4_1x8 port map(sel_cnt, S(7 downto 0), S(15 downto 8), S(23 downto 16), S(31 downto 24), seq_out_right);
+mux_seq_LEFT: Mux4_1x8 port map(sel_cnt, S(47 downto 40), S(55 downto 48), S(63 downto 56), S(71 downto 64), seq_out_left);
+
+
+
+-- Comparadores de igualdade
+
+comparador_RIGHT: Comparador port map(play_right(7 downto 0), play_right(15 downto 8), control_right);
+comparador_LEFT: Comparador port map(play_left(7 downto 0), play_left(15 downto 8), control_left);
+
+
+
+-- Lógicas combinacionais
+
+end_game <= end_time_left or end_time_right;
+pisca <= Sim_1Hz & Sim_1Hz & Sim_1Hz & Sim_1Hz & Sim_1Hz & Sim_1Hz & Sim_1Hz & Sim_1Hz & Sim_1Hz; -- CONFERIR SE FUNCIONA
+
+
+
+-- Divisor de frequência (deve ser modificado caso o projeto vá ser exportado para a DE2)
+
+rst_divfreq <= E1 or E5;
+divisorfreq_EMULADOR: Div_Freq_Emu port map(CLK, rst_divfreq, CLK_1Hz, Sim_1Hz);
 
 
 
